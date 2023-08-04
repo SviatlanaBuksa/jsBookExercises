08a4bba2c7ce148a382755bead2bf70987d1a398
Sviatlana Buksa <sviatlanabuksa@gmail.com>
2023-08-04 15:59:28 +0200

    Var,Inter,DataTypes

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 //JS FUNDAMENTALS
 
 //HELLO,WORLD!
-alert("I’m JavaScript!");
+//alert("I’m JavaScript!");
+
+//VARIABLES
+/* let admin;
+let name;
+
+name = "John";
+admin = name; */
+
+/* let ourPlanetName;
+let currentUserName;
+ */
+
+//DATA TYPES
+
+//Interaction: alert, prompt, confirm
+
+let currentUserName = prompt("What's your name");
+alert(`Your name is ${currentUserName}`);
