4ac89412cb6725716ba711f0d1b67c079d3bfbad
Sviatlana Buksa <sviatlanabuksa@gmail.com>
2023-08-07 16:04:19 +0200

    Comparisons

--- script.js
+++ script.js
@@ -18,5 +18,49 @@ let currentUserName;
 
 //Interaction: alert, prompt, confirm
 
-let currentUserName = prompt("What's your name");
-alert(`Your name is ${currentUserName}`);
+/* let currentUserName = prompt("What's your name");
+alert(`Your name is ${currentUserName}`); */
+
+//TYPE CONVERSIONS
+
+//BASIC OPERATORS, MATH
+
+/* let a = 1,
+  b = 1;
+
+let c = ++a; // ? 2 -c, a - 2
+let d = b++; // ? 1 -d, b- 2
+
+let a = 2;
+
+let x = 1 + (a *= 2); // a=4, x=5
+
+"" + 1 + 0; // '10'
+"" - 1 + 0; // -1
+true + false; // 1
+6 / "3"; // 2
+"2" * "3"; // 6
+4 + 5 + "px"; // '9px'
+"$" + 4 + 5; // '$45'
+"4" - 2; // 2
+"4px" - 2; // NaN
+"  -9  " + 5; // ' -9 5'
+"  -9  " - 5; // -14
+null + 1; // 1
+undefined + 1; // NaN
+" \t \n" - 2; // -2 
+
+let a = +prompt("First number?", 1);
+let b = +prompt("Second number?", 2);
+
+alert(a + b);*/
+
+//COMPARISONS
+
+/* 5 > 4 // true
+"apple" > "pineapple" // false
+"2" > "12" // !!!true
+undefined == null // true
+undefined === null // false
+null == "\n0\n" //false
+null === +"\n0\n" //false */
