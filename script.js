84aaa189fbce52e7df82cf89a7ac7aee4457c172
Sviatlana Buksa <sviatlanabuksa@gmail.com>
2023-08-10 23:24:06 +0200

    Switch statement

--- script.js
+++ script.js
@@ -178,11 +178,43 @@ do {
   num = prompt("Enter a number greater than 100?", 0);
 } while (num <= 100 && num); */
 
-let n = +prompt("enter an integer number", "10");
+/* let n = +prompt("enter an integer number", "10");
 
 outer: for (let i = 2; i <= n; i++) {
   for (let j = 2; j < i; j++) {
     if (i % j == 0) continue outer;
   }
   console.log(i);
+} */
+
+//THE SWITCH STATEMENT
+
+/* let browser = prompt("What is your browser?", "Edge");
+
+if (browser === "Edge") {
+  alert(`You've got the Edge!`);
+} else if (
+  browser === "Chrome" ||
+  browser === "Firefox" ||
+  browser === "Safari" ||
+  browser === "Opera"
+) {
+  alert("Okay we support these browsers too");
+} else {
+  alert("We hope that this page looks ok!");
+} */
+
+let a = +prompt("a?", "");
+
+switch (a) {
+  case 0:
+    alert(0);
+    break;
+  case 1:
+    alert(1);
+    break;
+  case 2:
+  case 3:
+    alert("2,3");
+    break;
 }
