897b3a1812d4328ad4c3bb4aa4cc578aae45cfb3
Sviatlana Buksa <sviatlanabuksa@gmail.com>
2023-09-02 15:45:07 +0200

    Array methods

--- script.js
+++ script.js
@@ -648,3 +648,46 @@ function getMaxSubSum(arr) {
 
   return maxSum;
 } */
+
+//Array methods
+
+//Translate border-left-width to borderLeftWidth
+/* function camelize(str) {
+  return str
+    .split("-")
+    .map((word, index) =>
+      index == 0 ? word : word[0].toUpperCase() + word.slice(1)
+    )
+    .join("");
+}
+
+alert(camelize("-webkit-transition")); */
+
+//Filter range
+/* let arr = [5, 3, 8, 1];
+
+let filtered = filterRange(arr, 1, 4);
+function filterRange(arr, a, b) {
+  return arr.filter((item) => item >= a && item <= b);
+}
+
+console.log(filtered);
+console.log(arr); */
+
+//Filter range "in place"
+function filterRangeInPlace(arr, a, b) {
+  for (let i = 0; i < arr.length; i++) {
+    let val = arr[i];
+
+    if (val < a || val > b) {
+      arr.splice(i, 1);
+      i--;
+    }
+  }
+}
+
+let arr = [5, 3, 8, 1];
+
+filterRangeInPlace(arr, 1, 4); // removed the numbers except from 1 to 4
+
+alert(arr); // [3, 1]
